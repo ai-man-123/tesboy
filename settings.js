5513ed2a222ea2b637fdd4efbcdcb989fdd202e8
AIMAN *͘⁴̅⁰͍⁴̵〆* <Lynxaiman@yahoo.com>
2023-01-21 10:36:50 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,8 +17,8 @@ global.APIKeys = {
 }
 
 // setting 
-global.autoread = false // auto read message
-global.antidelete = true // anti delete
+global.autoread = trur // auto read message
+global.antidelete = false // anti delete
 global.antiviewonce = true // anti view once
 global.autobio = false //auto ganti bio
 global.anticall = true //anti call 
@@ -33,24 +33,24 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/@ChZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Indonesia" //bebas
-global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.botname = "lynx-Mdོ" //namabot kalian
+global.ownername = "AimanOfcོ ×፝֟͜×" //nama kalian
+global.myweb = "https://lynxaiman.my.id" //bebas asal jan hapus
+global.youtube = "https://youtube.com" //bebas asal jan hapus
+global.myweb2 = "https://lynxaiman.my.id" //bebas
+global.email = "aimanmk1234@gmail.com" //bebas
+global.region = "Malaysia" //bebas
+global.timezone = 'Asia/Kuala_Lumpur' //  timezone wib
+global.premium = ['60102810046'] //premium user
+global.owner = ["60102810046"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "60102810046" // nomor wa kalian
+global.ownernomerr = "+60102810046" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
-global.donasi = "./image/donasi.jpg" // foto donasi di folder image
+global.donasi = "./image/lol.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© lynxbotmdོ' //sticker wm ubah
+global.author = 'Di Buat Oleh botlynx' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
@@ -124,7 +124,7 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 global.prefa = ['', '!', '.', '🐦', '🐤', '🗿']
 global.sp = '⭔'
 global.mess = {
-   sukses: '🤗Done, Oke Desu~',
+   sukses: 'Done, Oke Desu~',
    admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
